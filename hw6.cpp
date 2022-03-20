ac861511750e7ad8c4e51b2013fbfce3dc3aef33
wweidler <weidlerwill@gmail.com>
2022-03-19 22:55:42 -0500

    started assignment, realized we havent learned it

---
+++ hw6.cpp
@@ -0,0 +1,14 @@
+// Programmer: Will Weidler
+// ID: wawq97
+// Date: 3/19/22
+// File: hw6.cpp
+// Assignment: HW6
+// Purpose: This file contains a program that observes a bee outbreak.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
